036a6225b830dbec4f5dd5a688d13ba985c2b850
EECvision <eec.studies@gmail.com>
2023-02-14 22:50:54 +0100

    improve cardLoader component

--- components/LoadingScreen/CardLoader/CardLoader.jsx
+++ components/LoadingScreen/CardLoader/CardLoader.jsx
@@ -4,7 +4,9 @@ import Skeleton from "@mui/material/Skeleton";
 const CardLoader = ({ explore }) => {
   return (
     <div className={classes.container}>
-      <div style={{ width: explore ? "20em" : "100%" }} className={classes.wrapper}>
+      <div
+        style={{ minWidth: explore ? "20em" : "100%", width: "100%" }}
+        className={classes.wrapper}>
         <Skeleton animation="wave" variant="rounded" width="100%" height="100%" />
         <Skeleton animation="wave" variant="rounded" width="40%" height={30} />
         <Skeleton animation="wave" variant="rounded" width="70%" height={30} />
